6c6e5547d6a47014d96637a366dd2d453a371dcc
Colin Kupitz <kupitzc@gmail.com>
2016-06-07 11:24:22 -0700

    AXCPT Partially Complete

--- static/js/task.js
+++ static/js/task.js
@@ -647,6 +647,264 @@ var NBR_Task = function() {
 	nextblock();
 };
 
+/**************************************
+***************************************
+***************************************
+*****  		  AXCPT TASK  	 	  *****
+***************************************
+***************************************
+**************************************/
+var CPT_Task = function() {
+
+	// Experiment Control Variables
+	// TIMING VARIABLES (in ms):
+	var stimtime 		= 50; // CHANGEBACK 250;
+	var ISI 			= 50; // CHANGEBACK 1000;
+
+	// PRESENTATION VARIABLES:
+	var stimsize		= 80;
+	var stimcolor		= "black";
+
+	//TRIAL TYPES:
+	// 1 - target 			A->X	
+ 	// 2 - non-target lure	A->Y
+	// 3 - non-target X  	B->X
+	// 4 - pure distractor	B->Y
+	
+	//var trialtypes = [42, 6, 6, 6]; //CHANGEBACK
+	var trialtypes = [4, 1, 1, 1];
+
+	//TRIAL RECORDING VARIABLES
+	var letteron 	// time letter is presented
+	var letteroff	// time letter is removed
+	var lettertime  // total time letter is shown
+	var gotresp 	= false;
+	var respsame 	= -1;
+	var response 	= "noresp";
+	var listening 	= false;
+	var hit 		= -1;
+	var rt 			= -1; 
+
+	var maxblocks   = 1; //CHANGEBACK 6
+	var curblock	= 0;
+	var curtrial	= 0; //need my own trial counter
+
+	//STIMULUS VARIABLES
+	var stimletters = ["A","X","B","C","D","E","F","G","H","I","J",
+	"L","M","N","O","P","Q","R","S","T","U","V","W","Z"]; //from Jaeggi & Buschkuehl's verbatim
+	var nletters = stimletters.length;
+	//instantiate to set task scope
+	var stims 
+	var stimID
+	var letterID1
+	var letterID2
+
+	//CONTROLS INDIVIDUAL TRIALS
+	var nexttrial = function() {
+		if (stims.length===0) {
+			nextblock();
+		}
+		else {
+			// trial begin (reset everything)
+			gotresp 	= 0;
+			respsame 	= -1;
+			response 	= "noresp";
+			hit 		= -1;
+			rt 			= -1; 
+
+			curtrial 	= curtrial + 1; //increment trial
+
+			stim = stims.shift();
+			show_word( stim[0], stimcolor );
+			letteron = new Date().getTime();
+			listening = true; //essentially activates the response_handler()
+
+			//executes the code inside the anonymous function after a specified time
+			//e.g. this clears the stimulus after 'stimtime' ms
+			setTimeout(function() {
+    			remove_word();
+			}, stimtime);
+		}//else
+	};
+
+	//CONTROLS BLOCKS
+	var nextblock = function() {
+		if (curblock===maxblocks) {
+			finish();
+		}
+		else {
+			// block begin (prepare everything)
+
+			curblock		= curblock + 1; 		//increment block counter
+			curtrial 		= 0; 					//reset trial counter 
+
+			// easy setup of stimulus presentations
+			var tmpar1		= new Array(trialtypes[0]).fill(1);
+			var tmpar2		= new Array(trialtypes[1]).fill(2);
+			var tmpar3	 	= new Array(trialtypes[2]).fill(3);
+			var tmpar4	 	= new Array(trialtypes[3]).fill(4);
+
+			// not redeclaring vars - these are task scope variables
+			stimID 			= tmpar1.concat(tmpar2,tmpar3,tmpar4);
+			stimID 			= _.shuffle(stimID);
+			letterID1		= new Array(stimID.length).fill(0); //prefer numbers for data
+			letterID2		= new Array(stimID.length).fill(0); //prefer numbers for data 
+			stims 			= []; // reset to empty array
+
+			var ttype
+
+			// selects stimulus letters for each trial; nontargets are chosen randomly
+			for (i = 0; i < stimID.length; i++) {
+
+				letterID1[i] = Math.floor(2 + Math.random() * (nletters - 2));
+				letterID2[i] = Math.floor(2 + Math.random() * (nletters - 2));
+
+				//TRIAL TYPES:
+				switch (stimID[i]) { 
+					case 1: 	//  target 	A->X	
+						letterID1[i] = 0; //A
+						letterID2[i] = 1; //X
+						ttype = "target";
+						break;
+					case 2: 	//  on-target lure	A->Y
+						letterID1[i] = 0; //A
+						letterID2[i] = Math.floor(2 + Math.random() * (nletters - 2));
+						ttype = "nontarget";
+						break;
+					case 3: 	//  non-target X  	B->X
+						letterID1[i] = Math.floor(2 + Math.random() * (nletters - 2));
+						letterID2[i] = 1; //X
+						ttype = "nontarget";
+						break;
+					case 4: 	//  pure distractor	B->Y
+						letterID1[i] = Math.floor(2 + Math.random() * (nletters - 2));
+						letterID2[i] = Math.floor(2 + Math.random() * (nletters - 2));
+						ttype = "nontarget";
+						break;
+					default:
+						letterID1[i] = -1;
+						letterID2[i] = -1;
+						ttype = "broken";
+						break;
+				}
+
+				stims.push([ 	stimletters[letterID1[i]] , stimletters[letterID2[i]],
+				 				ttype, letterID1[i], letterID2[i], stimID[i] ]);
+
+			} //for
+
+			var tmpphase = "blocksetup";
+  			psiTurk.recordTrialData({	'phase': 	tmpphase,
+  										'task': 	curtask,
+    									'block': 	curblock,
+                                     	'stimID': 	stimID,
+                                     	'letterID1': letterID1,
+                                     	'letterID2': letterID2
+                                     });
+                                     	// 'tmpar1': 	tmpar1,
+                                     	// 'tmpar2': 	tmpar2,
+                                     	// 'tmpar3': 	tmpar3,
+                                     	// 'tmpar4': 	tmpar4,
+			nexttrial(); //runs every trial
+		} //else
+	};
+
+	var response_handler = function(e) {
+		if (!listening) return;
+
+		var keyCode = e.keyCode;
+
+		/// f = 70, j = 74
+		// space = 32
+		switch (keyCode) {
+			case 70: 	// "F"
+				response 	= Fresp;
+				respsame 	= Frespsame;
+				// response = "target";
+				// respsame = 1;
+				break;
+			case 74: 	// "J"
+				response 	= Jresp;
+				respsame 	= Jrespsame;
+				// response = "nontarget";
+				// respsame = 0;
+				break;
+			default:
+				response = "";
+				respsame = -1; //redundant but w/e
+				break;
+		}
+		if (response.length>0) {
+			listening 	= false;
+			gotresp 	= 1;
+			hit 		= response == stim[2];
+			rt 			= new Date().getTime() - letteron;
+
+		} // if (response.length>0) {
+	}; //response_handler
+
+	var finish = function() {
+	    $("body").unbind("keydown", response_handler); // Unbind keys
+	    currentview = new Task_Controller();
+	};
+	
+	var show_word = function(text, color) {
+		d3.select("#stim")
+			.append("div")
+			.attr("id","letter")
+			.style("color",color)
+			.style("text-align","center")
+			.style("font-size","150px")
+			.style("font-weight","400")
+			.style("margin","20px")
+			.text(text);
+	};
+
+	var remove_word = function() {
+		// the 'letter' attribute is set in the show_word function
+		d3.select("#letter").remove();
+		letteroff = new Date().getTime();
+		lettertime = letteroff - letteron;
+
+		// after ISI ms, record the current trial data, move to next trial
+		//THIS IS IN JSON FORMAT
+		setTimeout(function() {
+    			// psiTurk.recordTrialData({	'phase': 		curphase,
+    			// 							'task': 		curtask,
+    			// 							'block': 		curblock,
+    			// 							'trial': 		curtrial,
+       //                              		'letter': 		stim[0],
+       //                               		'stimType': 	stim[1],
+       //                               		'letterID': 	stim[2],
+       //                               		'stimID': 		stim[3],
+       //                               		'response': 	response,
+       //                               		'respsame': 	respsame,
+       //                               		'lettertime': 	lettertime,
+       //                               		'hit': 			hit,
+       //                               		'rt': 			rt
+       //                               	});
+       
+      			psiTurk.recordTrialData([curphase,curtask,curblock,curtrial,stim[0],stim[1],stim[2],stim[3],
+       			response,respsame,lettertime,hit,rt]);
+    			nexttrial();
+		}, ISI);
+	};
+
+	
+	// Load the stage.html snippet into the body of the page
+	psiTurk.showPage('stage.html');
+
+	//basic task reminder at top
+	d3.select("#query").html('<p id="prompt">2-back. F = Target, J = Non-Target.</p>');
+
+	// Register the response handler that is defined above to handle any
+	// key down events.
+	$("body").focus().keydown(response_handler); 
+
+	// Start the test
+	nextblock();
+};
+
 
 /****************
 * Questionnaire *
@@ -748,7 +1006,7 @@ var curtaskID
 var curphase 	 = "Main";
 var curtaskIndex = 0;
 
-taskorder = [1, 2]; //CHANGEBACK --REMOVEME!
+taskorder = [1, 2, 3]; //CHANGEBACK --REMOVEME!
 
 /*******************
  * Run Task
