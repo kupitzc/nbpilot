7dc7d962a2bd22aca2572a944b5021a6017baf5d
kupitzc <kupitzc@gmail.com>
2016-07-04 03:52:07 -0700

    done checking

--- static/js/task.js
+++ static/js/task.js
@@ -8,8 +8,8 @@
 var psiTurk = new PsiTurk(uniqueId, adServerLoc, mode);
 
 var isdebugrun 		= 0; 	//SET ME TO 0 FOR REAL RUN!
-var istimedebugrun 	= 1;
-var isrespdebugrun 	= 1;
+var istimedebugrun 	= 0;
+var isrespdebugrun 	= 0;
 //alert(mycondition);
 //alert(mycounterbalance);
 
